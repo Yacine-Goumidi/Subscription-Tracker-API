bd5cdc9e2fd666c98842ef581a46e0ee99567397
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-01-18 23:35:53 +0100

    Create Express Server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log ('server running on localhost:3000');
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Tracker API');
+});
+
+
+app.listen(3000, () => {
+    console.log('Subscription Tracker API is Running on http://localhost:3000');
+})
+
+export default app;
