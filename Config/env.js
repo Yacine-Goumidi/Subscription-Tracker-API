39f910f499a2162ddf53f502767955219eb92741
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-01-19 11:35:25 +0100

    Config

---
+++ Config/env.js
@@ -0,0 +1,9 @@
+﻿import { config } from 'dotenv';
+
+// TypeScript-style options
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+// Destructuring pour PORT
+const { PORT, Node_ENV } = process.env;
+
+export { PORT };
