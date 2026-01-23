8aa4741e35630edec5bcbc95ab4e5aa924681219
Yacine-Goumidi <yacine.zeref@gmail.com>
2026-01-23 23:43:07 +0100

    errorMiddleware Update

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { PORT } from './config/env.js';
 import authRouter from './Routes/auth.routes.js';
 import userRouter from './Routes/user.routes.js';
 import subscriptionRouter from './Routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import ErrorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use(ErrorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription Tracker API!');
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,1 +1,34 @@
-﻿const errorHandler = (err, req, res, next) => {}
+﻿const errorHandler = (err, req, res, next) => {
+    let error = { ...err };
+    error.message = err.message;
+
+    console.error(err);
+
+    // Mongoose bad ObjectId
+    if (err.name === 'CastError') {
+        error = new Error('Resource not found');
+        error.statusCode = 404;
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        error = new Error('Duplicate field value entered');
+        error.statusCode = 400;
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors)
+            .map(val => val.message)
+            .join(', ');
+        error = new Error(message);
+        error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || 'Server Error'
+    });
+};
+
+export default errorHandler;
